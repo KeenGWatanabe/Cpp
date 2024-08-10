be9d5caf084b29d1d1ed797683ee1d4010287d9c
60936 <studio34pl@gmail.com>
2024-08-10 19:40:57 +0800

    r

---
+++ cSharp/C#Lesson2/Program.cs
@@ -0,0 +1,10 @@
+﻿public class MyFirstApp
+{
+    public static void Main()
+    {
+        int a = 1;    
+        int b = 2;
+        int c = a + b;
+        Console.WriteLine("Result is : " + c);
+    }
+}
---
+++ cSharp/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string name = "Tech";
+        int number = 2;
+        float price = 3.99f;
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello " + name);
+        Console.WriteLine("this is numeric data ");
+        Console.WriteLine(2+2);
+        Console.WriteLine(number * price);
+
+    }
+}
+// stop at 32:26 
