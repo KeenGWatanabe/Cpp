b4fb18bb5d2af8932e493822784d9bb2da585de7
KeengWatanabe <mckeenweb@gmail.com>
2024-08-11 10:51:35 +0800

    r

--- cSharp/C#Lesson2/Program.cs
+++ cSharp/C#Lesson2/Program.cs
@@ -2,9 +2,16 @@
 {
     public static void Main()
     {
-        int a = 1;    
+        int a = 1;    // initialise a as 1 in definition
         int b = 2;
+
+        a = 5;  // chg a to 5, assigning value to definition
         int c = a + b;
         Console.WriteLine("Result is : " + c);
+
+        string username = "Default";
+        username = "whatever user has typed"; //practical use of var assignment
+        Console.WriteLine(username);
+
     }
 }
